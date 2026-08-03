deebc2743bac081687f68a4ea90efacc57e5700f
HaimMeiriEZ <haim.m@ez-roi.com>
2026-08-03 15:29:29 +0300

    feat: test clickup auto creation v3

--- clickup_test.py
+++ clickup_test.py
@@ -1,8 +1,9 @@
 def check_status(user_role: str) -> bool:
-    """פונקציית בדיקה לבדיקת אוטומציית ClickUp v2"""
+    """פונקציית בדיקה לבדיקת אוטומציית ClickUp v3"""
     if user_role == "admin":
         return True
     return False
 
 # בדיקה נוספת
-print("ClickUp v2 test active")
+print("ClickUp v3 test active")
+
